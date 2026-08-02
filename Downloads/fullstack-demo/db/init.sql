57e6fc85137c95f01fba37b398ce109d3e57faab
Suriya Prashad <K02689@tvs.in>
2026-08-02 12:47:29 +0530

    Initial commit

---
+++ Downloads/fullstack-demo/db/init.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE demo_db; USE demo_db; CREATE TABLE users(id INT PRIMARY KEY AUTO_INCREMENT,name VARCHAR(100),email VARCHAR(100)); INSERT INTO users(name,email) VALUES ('Suriya','suriya@example.com');
